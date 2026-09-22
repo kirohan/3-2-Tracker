a031eb00d8fe98354dc2b9b056e28bcc81172d4c
K.I.Rohan <kirohan95@gmail.com>
2026-09-23 01:43:30 +0600

    zero

--- config.js
+++
@@ -1,9 +0,0 @@
-window.TRACKER_CONFIG = {
-  // One-time setup: paste the share URL of the Google Sheets template here.
-  // Example: https://docs.google.com/spreadsheets/d/XXXXXXXXXXXX/edit?usp=sharing
-  // The site automatically converts it to a /copy URL so visitors can save a personal copy to their Google Drive.
-  googleSheetTemplateUrl: "",
-  excelDownloadUrl: "./assets/BECM_2K22_3-2_Attendance_Tracker.xlsx",
-  creator: "K.I.Rohan",
-  studio: "Bad Time Studio"
-};
